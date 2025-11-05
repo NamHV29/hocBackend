118a2842b36c7adef18497e18eb9c7379fa4c3e3
Hoang Viet Nam <hoangnamyma@gmail.com>
2025-11-05 16:39:59 +0700

    commit lan 3

--- hocBackend/Program.cs
+++ hocBackend/Program.cs
@@ -11,6 +11,33 @@ namespace hocBackend
         static void Main(string[] args)
         {
             Console.WriteLine("xin chao");
+            int thang;
+            thang = 4;
+            switch (thang)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    Console.WriteLine("31 ngay");
+                        break;
+                case 2:
+                    Console.WriteLine("29 ngay");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    Console.WriteLine("30 ngay");
+                    break;
+                default:
+                    break;
+
+            }
+            Console.ReadLine();
         }
     }
 }
